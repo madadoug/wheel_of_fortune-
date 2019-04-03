031304e0e9f6936cb4c9992dc3e269b9f1e1352a
madadoug <47403840+madadoug@users.noreply.github.com>
2019-04-03 17:11:50 -0500

    Add files via upload

---
+++ wheel_of_fortune.js
@@ -0,0 +1,17 @@
+var word = "WHEEL";
+var isGuessingLetter = true;
+var letter = "W";
+
+if (letter = true) {
+    console.log("W _ _ _ _");
+} else {
+    console.log ("Sorry, no " + letter + "!");
+}
+
+if (isGuessingLetter != false) {
+		console.log("Congratulations! You won Wheel Of Fortune!");
+} else {
+ console.log("Nope, keep trying!");
+
+}
+	
